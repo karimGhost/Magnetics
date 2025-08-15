4dd11924750e6f460bf2f9781cf4c0c9b6bec17f
karimGhost <abdulkarimkassimsalim@gmail.com>
2025-08-16 02:07:56 +0300

    Deploy build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,10 @@ import type { Config } from "tailwindcss";
 
 export default {
     darkMode: ["class"],
+	 safelist: [
+    '-mt-[150px]',
+    'lg:-mt-[200px]',
+  ],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
