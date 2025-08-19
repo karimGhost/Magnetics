869e7423a9e7c1a6f8f52f1b851b637a629c8784
karimGhost <abdulkarimkassimsalim@gmail.com>
2025-08-19 03:22:13 +0300

    Deploy build

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -14,7 +14,7 @@ export default function BlogPage() {
 
 
   return (
-    <div className="min-h-screen flex flex-col mt-5" style={{marginTop:"90px"}}>
+    <div className="min-h-screen flex flex-col " style={{marginTop:"-130px"}}>
    
 
 
--- src/components/GoogleAd.jsx
+++ src/components/GoogleAd.jsx
@@ -5,19 +5,26 @@ import { useEffect } from 'react';
 
 export default function GoogleAd() {
   useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error('AdSense error:', e);
-    }
+    const timer = setTimeout(() => {
+      try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) {
+        console.error('AdSense error:', e);
+      }
+    }, 500); // small delay so script loads
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <ins className="adsbygoogle"
-         style={{ display: 'block' }}
-         data-ad-client="ca-pub-1948682636540486"
-         data-ad-slot="2569180712"
-         data-ad-format="auto"
-         data-full-width-responsive="true" />
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-1948682636540486"
+      data-ad-slot="2569180712" // 👈 slot from AdSense dashboard
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 }
+
